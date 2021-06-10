057ebd97f09407259c20370394cbe538b276b82e
leonardcad02 <leonardcad02@gmail.com>
2021-06-09 19:19:14 -0500

    Iniciando Aplicacion

---
+++ src/main/java/com/pokedexApiRest/PokedexApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.pokedexApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokedexApiRest/PokedexApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokedexApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
